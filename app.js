f19bd619915bdb2cdfe59ed57e0c9b0a3f276ea3
Marina Zelenevskaya <mzelianeuskaya@yandex.ru>
2023-03-24 20:36:59 +0300

    commit9

--- app.js
+++ app.js
@@ -6,5 +6,5 @@
 let pageIdHeader = document.querySelector("[data-id-page-header]").getAttribute("data-id-page-header"),
     navItemHeader = document.querySelector(`[data-id-nav-header=${pageIdHeader}]`);
 if (pageIdHeader == navItemHeader.getAttribute("data-id-nav-header")) {
-    navItemHeader.classList.add("active");hhh111222666yytttyyy
+    navItemHeader.classList.add("active");hhh111222666yytttyyy555
 }
